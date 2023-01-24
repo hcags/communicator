8bd03dcd99c0152721a65ce370819a11e0240ff5
hcags <120849679+hcags@users.noreply.github.com>
2023-01-24 23:02:26 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-
+input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function () {
+    radio.sendString("Ich bin Master Steve")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
+    radio.sendString("Guten Tag ihr Minimenschen")
+})
+input.onPinTouchEvent(TouchPin.P2, input.buttonEventDown(), function () {
+    radio.sendString("Ihr dürft mich Captian nennen")
+})
+input.onPinTouchEvent(TouchPin.P3, input.buttonEventDown(), function () {
+    radio.sendString("Und jetzt Arbeitet!")
+})
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
