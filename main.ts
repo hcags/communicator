26dce59cad10f7c060c2186e90136b3deb9e5c32
hcags <120849679+hcags@users.noreply.github.com>
2023-01-25 14:52:38 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,19 @@
+input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function () {
+    radio.sendString("Ich mag Katzen ")
+})
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 	
 })
 radio.onReceivedString(function (receivedString) {
-	
+    basic.showString(receivedString)
+})
+input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
+    radio.sendString("Cool")
+})
+input.onPinTouchEvent(TouchPin.P2, input.buttonEventDown(), function () {
+    radio.sendString("Arne ist mein freund")
 })
 input.onPinTouchEvent(TouchPin.P3, input.buttonEventDown(), function () {
-	
+    radio.sendString("Alle")
 })
+radio.setGroup(1)
