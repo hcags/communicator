87132c9ffe33f8983a57ba38d8d5a24c3a94e45e
hcags <120849679+hcags@users.noreply.github.com>
2023-03-01 14:53:12 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,13 @@ input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function () {
     radio.sendString("Ihr")
 })
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-	
+    if (Funkgruppe == 1) {
+        Funkgruppe = 9
+    } else {
+        Funkgruppe += -1
+    }
+    basic.showNumber(Funkgruppe)
+    radio.setGroup(Funkgruppe)
 })
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
@@ -16,4 +22,5 @@ input.onPinTouchEvent(TouchPin.P2, input.buttonEventDown(), function () {
 input.onPinTouchEvent(TouchPin.P3, input.buttonEventDown(), function () {
     radio.sendString("Kleinen Menschen")
 })
-radio.setGroup(1)
+let Funkgruppe = 0
+Funkgruppe += 1
