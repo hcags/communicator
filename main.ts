06625a566a0d70bdd277415f7ae611062b9cea0a
hcags <120849679+hcags@users.noreply.github.com>
2023-03-07 22:05:27 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
 input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function () {
-    radio.sendString("RUHE!!!")
+    sendString("wie gehts?")
 })
+function sendString (sendText: string) {
+    radio.sendString(sendText)
+    basic.setLedColor(0x00ff00)
+    basic.pause(500)
+    basic.setLedColor(0xffff00)
+}
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     if (Funkgruppe == 1) {
         Funkgruppe = 9
@@ -10,18 +16,30 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     basic.showNumber(Funkgruppe)
     radio.setGroup(Funkgruppe)
 })
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    if (Funkgruppe == 9) {
+        Funkgruppe = 1
+    } else {
+        Funkgruppe += 1
+    }
+    basic.showNumber(Funkgruppe)
+    radio.setGroup(Funkgruppe)
+})
 radio.onReceivedString(function (receivedString) {
+    basic.setLedColor(0xff0000)
     basic.showString(receivedString)
+    basic.setLedColor(0xffff00)
 })
 input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
-    radio.sendString("Guten Tag")
+    sendString("Hi du :)")
 })
 input.onPinTouchEvent(TouchPin.P2, input.buttonEventDown(), function () {
-    radio.sendString("LOS GEHTS!!!")
+    sendString("Was machst du?")
 })
 input.onPinTouchEvent(TouchPin.P3, input.buttonEventDown(), function () {
-    radio.sendString("Heute Funken wir")
+    sendString("Tschau")
 })
 let Funkgruppe = 0
 Funkgruppe = 1
 radio.setGroup(Funkgruppe)
+basic.setLedColor(0xffff00)
